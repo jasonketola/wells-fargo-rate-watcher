fa34fb5e1d07a28aa8b127cb342dcb9519bbee1f
jasonketola <jasonketola@gmail.com>
2021-02-28 17:56:24 +0000

    Prototype.

--- src/wells-fargo-rate-watcher.go
+++ src/wells-fargo-rate-watcher.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	
-
-
+	"strconv"
+	"log"
+	"os"
 	"github.com/gocolly/colly"
+	"github.com/joho/godotenv"
 )
 
 
@@ -12,7 +13,10 @@ import (
 
 func main() {
 
-
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env, %v", err)
+	}
 
 	// Instantiate default collector
 	c := colly.NewCollector(
@@ -22,23 +26,55 @@ func main() {
 
 
 
-
-
-
 	var rates []string
 
 	// On every a HTML element which has name attribute call callback
 	c.OnHTML(`table[class="trTable trassumptiontable"] tbody tr`, func(e *colly.HTMLElement) {
-		// Activate detailCollector if the class contains "product-link product-thumbnail"
+		// Grab all the data from these cells. The first one will be the interest rate.
 		rates = append(rates, e.ChildText("td"))
 	})
 
 
 
-
+	// Page for 30 year conforming loans
 	myURL := "https://www.wellsfargorelo.com/relo/todaysRates_assumptions.page?loanPurpose=Refinance&loanType=1&productName=30-Year+Fixed+Rate&suffix=yourcompany1096"
 	c.Visit(myURL)
 
-	println(rates[0])
-	
+	// Only do anything if colly successfully got some results back
+	if len(rates) > 0 {
+		// Trim the percent symbol off and convert string to a float
+		stringRate := rates[0][:len(rates[0])-1]
+		currentRate, _ := strconv.ParseFloat(stringRate, 32)
+
+		if currentRate < 3.0 {
+			emailRate(stringRate)
+		} else {
+			return
+		}
+	}
+}
+
+func emailRate(rate string) {
+	host := os.Getenv(EMAIL_HOST)
+	from := os.Getenv(EMAIL_FROM)
+	password := os.Getenv(EMAIL_PASSWORD)
+	port := os.Getenv(EMAIL_PORT)
+	to := os.Getenv(EMAIL_TO)
+
+	msg := "From: " + from + "\n" +
+	"To: " + to + "\n" +
+	"Subject: Attractive Wells Fargo interest rate - " + rate + "\n\n" +
+	"Think about refinancing, please."
+
+	addr := fmt.Sprintf("%s:%s", host, port)
+
+	err := smtp.SendMail(addr,
+		smtp.PlainAuth("", from, password, host),
+		from, []string{to}, []byte(msg))
+
+	if err != nil {
+		log.Printf("SMTP error: %s", err)
+		return
+	}
+
 }
